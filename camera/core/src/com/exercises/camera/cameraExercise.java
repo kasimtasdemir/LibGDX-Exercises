e48756704dd8c761d5bec93cc3515a93ac073747
aspirintr <kasimtasdemir@gmail.com>
2019-12-21 19:57:05 +0300

    move rotate zoom camera

--- camera/core/src/com/exercises/camera/cameraExercise.java
+++ camera/core/src/com/exercises/camera/cameraExercise.java
@@ -2,6 +2,7 @@ package com.exercises.camera;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -37,6 +38,16 @@ public class cameraExercise extends ApplicationAdapter {
     public void render () {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        float delta = Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            overviewCamera.translate(10 * delta,0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.Z)){
+            overviewCamera.zoom -= 0.1 * delta;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.R)){
+            overviewCamera.rotate(1 * delta);
+        }
         overviewCamera.update();
         batch.setProjectionMatrix(overviewCamera.combined);
         batch.begin();
