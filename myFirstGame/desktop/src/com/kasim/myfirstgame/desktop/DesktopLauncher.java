66dadc32bbef60e19364acd1d5338be7809610e8
aspirintr <kasimtasdemir@gmail.com>
2020-03-15 17:23:44 +0300

    myFirstGame first build. Empty project.

---
+++ myFirstGame/desktop/src/com/kasim/myfirstgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kasim.myfirstgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kasim.myfirstgame.myFirstGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new myFirstGame(), config);
+	}
+}
