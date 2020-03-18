3346a6d9e13e42a26436e515aead3d5a544860e8
aspirintr <kasimtasdemir@gmail.com>
2020-03-18 22:47:12 +0300

    rotating test boxes are added. (Upto Ch3, p126)

--- myFirstGame/core/src/com/kasim/myfirstgame/game/WorldRenderer.java
+++ myFirstGame/core/src/com/kasim/myfirstgame/game/WorldRenderer.java
@@ -1,19 +1,37 @@
 package com.kasim.myfirstgame.game;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Disposable;
+import com.kasim.myfirstgame.util.Constants;
 
 public class WorldRenderer implements Disposable {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private WorldController worldController;
-    public WorldRenderer (WorldController worldController) { }
-    private void init () { }
-    public void render () { }
-    public void resize (int width, int height) { }
+    public WorldRenderer (WorldController worldController) {
+        this.worldController = worldController;
+        init();
+    }
+    private void init () {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH,
+                Constants.VIEWPORT_HEIGHT); camera.position.set(0, 0, 0); camera.update();
+    }
+    public void render () {
+        renderTestObjects();
+    }
+    private void renderTestObjects() { batch.setProjectionMatrix(camera.combined); batch.begin();
+        for(Sprite sprite : worldController.testSprites) {
+            sprite.draw(batch); }
+        batch.end(); }
+    public void resize (int width, int height) {
+        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT / height) * width;
+        camera.update();
+    }
     @Override
     public void dispose() {
-
+        batch.dispose();
     }
 }
