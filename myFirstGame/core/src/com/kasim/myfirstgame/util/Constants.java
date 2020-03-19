b9982eef81b5c9d2fd8961a941770736b017bd5e
aspirintr <kasimtasdemir@gmail.com>
2020-03-19 19:32:50 +0300

    Assets class template is added. No texture is loaded yet.

--- myFirstGame/core/src/com/kasim/myfirstgame/util/Constants.java
+++ myFirstGame/core/src/com/kasim/myfirstgame/util/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
     public static final float VIEWPORT_WIDTH = 5.0f;
     // Visible game world is 5 meters tall
     public static final float VIEWPORT_HEIGHT = 5.0f;
+    public static final String TEXTURE_ATLAS_OBJECTS =
+            "images/canyonbunny.pack";
 }
